43b298cd0dc48c01323545b91281863585384052
nobiemina <ocemina@cougarnet.uh.edu>
2025-04-16 21:34:05 -0500

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -54,7 +54,7 @@ function validatefirstname()
     document.getElementById("fname_message").innerHTML = "First name needs to contain at least 2 characters";  
     error_flag = 1;
     }
-
+  else if {
     if (x.match(/[a-zA-Z2-5'-]+$/.test(x))) {
     document.getElementById("fname_message").innerHTML = "";  
     }
